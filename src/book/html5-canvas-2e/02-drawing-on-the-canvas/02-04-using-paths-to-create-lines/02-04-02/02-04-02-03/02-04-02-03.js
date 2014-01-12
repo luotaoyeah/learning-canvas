95a2a7978331e3e81466a1727b83ee10f0efaf1a
luotao <luotaoyeah@gmail.com>
2014-01-12 12:29:49 +0800

    html5-canvas: 02-04: path: lineWidth

---
+++ src/book/html5-canvas-2e/02-drawing-on-the-canvas/02-04-using-paths-to-create-lines/02-04-02/02-04-02-03/02-04-02-03.js
@@ -0,0 +1,49 @@
+/*
+ * 2.4. Using Paths to Create Lines
+ *     2.4.3. lineWidth
+ */
+
+window.addEventListener(
+  "load",
+  function() {
+    var canvas = document.getElementById("canvas01");
+    if (canvas && canvas.getContext) {
+      var ctx = (function(canvas) {
+        var dpr = window.devicePixelRatio || 1;
+        var rect = canvas.getBoundingClientRect();
+        canvas.width = rect.width * dpr;
+        canvas.height = rect.height * dpr;
+        var ctx = canvas.getContext("2d");
+        ctx.scale(dpr, dpr);
+        return ctx;
+      })(canvas);
+      var width = canvas.width / (window.devicePixelRatio || 1);
+      var height = canvas.height / (window.devicePixelRatio || 1);
+      console.log("--------------------------------------------------");
+
+      drawScreen(ctx, width, height);
+    }
+
+    /**
+     *
+     * @param {CanvasRenderingContext2D} ctx
+     * @param width
+     * @param height
+     */
+    function drawScreen(ctx, width, height) {
+      /*
+       * lineWidth 属性，用于设置线条的粗细，默认为 1.0
+       */
+
+      ctx.strokeStyle = "#000000";
+      ctx.lineWidth = 20;
+
+      ctx.beginPath();
+      ctx.moveTo(100, 100);
+      ctx.lineTo(200, 200);
+
+      ctx.stroke();
+    }
+  },
+  false
+);
