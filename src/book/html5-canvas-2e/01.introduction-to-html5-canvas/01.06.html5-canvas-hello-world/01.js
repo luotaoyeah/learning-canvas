2cfd6d712bbde2e8eafbb8e47c1444020813e1b1
luotao <luotaoyeah@gmail.com>
2014-01-22 21:55:32 +0800

    feat(html5-canvas): 01.06.html5-canvas-hello-world

--- src/book/html5-canvas-2e/01.introduction-to-html5-canvas/01.06.html5-canvas-hello-world/01.js
+++ src/book/html5-canvas-2e/01.introduction-to-html5-canvas/01.06.html5-canvas-hello-world/01.js
@@ -1,17 +1,17 @@
 /*
- * 对 canvas 的操作必须保证在文档加载完成之后进行，
+ * 对 canvas 的操作必须保证在文档加载完成之后进行,
  * 因此需要监听 window 对象的 load 事件
  */
 
 window.addEventListener(
   'load',
-  function() {
+  function () {
     var canvas = document.getElementById('canvas01');
     if (canvas && canvas.getContext) {
       /*
-       * 通过 HTMLCanvasElement.getContext() 方法，
-       * 获取 canvas 元素的 context 对象，
-       * 对 canvas 元素的所有操作，都是通过 context 对象进行的
+       * 通过 HTMLCanvasElement.getContext() 方法,
+       * 获取 canvas 元素的 context 对象,
+       * 对 canvas 元素的所有操作, 都是通过 context 对象进行的
        */
       var context = canvas.getContext('2d');
       drawScreen(context);
@@ -24,25 +24,25 @@ window.addEventListener(
     function drawScreen(ctx) {
       /* 设置背景颜色 */
       ctx.fillStyle = '#ffffaa';
-      ctx.fillRect(0, 0, 1000, 800);
+      ctx.fillRect(0, 0, 1024, 720);
 
       /* 添加文本 */
       ctx.fillStyle = '#000000';
       ctx.font = "20px '宋体'";
       ctx.textBaseline = 'top';
       ctx.textAlign = 'center';
-      ctx.fillText('hello world', 500, 100);
+      ctx.fillText('HELLO WORLD', 512, 100);
 
       /* 添加图片 */
       var image = new Image(418, 435);
-      image.onload = function() {
-        ctx.drawImage(image, 500 - 418 / 2, 400 - 435 / 2);
+      image.onload = function () {
+        ctx.drawImage(image, 1024 / 2 - 418 / 2, 720 / 2 - 435 / 2);
       };
-      image.src = './01-06-01.jpg';
+      image.src = './01.jpg';
 
       /* 添加边框 */
       ctx.strokeStyle = '#000000';
-      ctx.strokeRect(0, 0, 1000, 800);
+      ctx.strokeRect(0, 0, 1024, 720);
     }
   },
   false,
