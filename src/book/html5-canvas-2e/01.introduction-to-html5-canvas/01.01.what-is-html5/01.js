25185d7358b3d77ec8e6cb3ef517de56012b7fef
luotao <luotaoyeah@gmail.com>
2014-01-22 19:18:31 +0800

    feat(html5-canvas): 01.01 what-is-html5

---
+++ src/book/html5-canvas-2e/01.introduction-to-html5-canvas/01.01.what-is-html5/01.js
@@ -0,0 +1,13 @@
+/*
+ * Introduction to HTML5 Canvas
+ *     What Is HTML5?
+ */
+
+/*
+ * 从小的方面来说, HTML5 指的就是 HTML 规范的第五个版本
+ * 从大的方面来说, HTML5 指的是开放网络平台（open web platform）
+ *     它包括一系列的技术：
+ *         HTML5, CSS3, SVG, MathML, XmlHttpRequest, Context 2D, Web Fonts 等等
+ *
+ * HTML5 中的 canvas 就是用来替代以前的 flash 的功能
+ */
