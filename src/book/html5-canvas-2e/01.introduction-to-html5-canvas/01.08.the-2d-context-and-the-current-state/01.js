70d5ee8a057596ec7b9440b316292b7e41a3470e
luotao <luotaoyeah@gmail.com>
2014-01-23 20:08:01 +0800

    feat(html5-canvas): 01.08.the-2d-context-and-the-current-state

---
+++ src/book/html5-canvas-2e/01.introduction-to-html5-canvas/01.08.the-2d-context-and-the-current-state/01.js
@@ -0,0 +1,39 @@
+window.addEventListener(
+  'load',
+  function () {
+    drawScreen(document.getElementById('canvas01').getContext('2d'));
+
+    /**
+     *
+     * @param {CanvasRenderingContext2D} ctx
+     */
+    function drawScreen(ctx) {
+      /*
+       * context 是一个 CanvasRenderingContext2D 对象,
+       * 对 canvas 的所有操作都是通过 context 对象进行的,
+       * context 使用笛卡尔（cartesian）坐标系, 左上角是坐标原点（0, 0）
+       */
+
+      /*
+       * 因为 canvas 是属于 immediate mode 而不是 retained mode
+       * 当某个部分发生变化时, 整个画布都需要重绘,
+       * 因此, canvas 中有一个 current state 的概念
+       *
+       * 当前状态:
+       *     指的就是是当前 context 对象上面的一系列属性设置,
+       *     这些属性状态对整个画布生效,
+       *     在每次绘制操作之前, 都需要先对这些状态属性进行设置
+       * 当前状态包括如下属性:
+       *     strokeStyle, fillStyle, lineWidth, 等等
+       */
+
+      ctx.fillStyle = '#ffffaa';
+      ctx.fillRect(0, 0, 1024, 720);
+
+      ctx.strokeStyle = '#ff0000';
+      ctx.lineWidth = 2;
+      ctx.strokeRect(1, 1, 1024 - 2, 720 - 2);
+    }
+  },
+  false,
+);
