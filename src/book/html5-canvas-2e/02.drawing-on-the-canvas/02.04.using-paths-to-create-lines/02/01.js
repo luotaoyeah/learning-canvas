9fde055baa6ceeef38eaafa15d6e5e41af8a7fde
luotao <luotaoyeah@gmail.com>
2014-02-06 21:05:25 +0800

    feat(html5-canvas): 02.04.02 the-actual-drawing: lineCap

--- src/book/html5-canvas-2e/02.drawing-on-the-canvas/02.04.using-paths-to-create-lines/02/01.js
+++ src/book/html5-canvas-2e/02.drawing-on-the-canvas/02.04.using-paths-to-create-lines/02/01.js
@@ -5,10 +5,10 @@
 
 window.addEventListener(
   'load',
-  function() {
+  function () {
     var canvas = document.getElementById('canvas01');
     if (canvas && canvas.getContext) {
-      var ctx = (function(canvas) {
+      var ctx = (function (canvas) {
         var dpr = window.devicePixelRatio || 1;
         var rect = canvas.getBoundingClientRect();
         canvas.width = rect.width * dpr;
@@ -19,46 +19,70 @@ window.addEventListener(
       })(canvas);
       var width = canvas.width / (window.devicePixelRatio || 1);
       var height = canvas.height / (window.devicePixelRatio || 1);
-      console.log('--------------------------------------------------');
 
       drawScreen(ctx, width, height);
     }
 
     /**
+     * lineCap 属性, 用于设置线条端点的形状,
+     * 可选属性值
+     *    butt（默认）
+     *    square      矩形
+     *    round       半圆
      *
      * @param {CanvasRenderingContext2D} ctx
      * @param width
      * @param height
      */
     function drawScreen(ctx, width, height) {
-      /*
-       * lineCap 属性，用于设置线条顶端的样式，
-       * 可选属性值：butt（默认），square，round
-       */
-
+      ctx.lineWidth = 50;
       ctx.strokeStyle = '#000000';
-      ctx.lineWidth = 10;
 
       ctx.beginPath();
-      ctx.moveTo(10, 10);
-      ctx.lineTo(110, 10);
+      ctx.moveTo(100, 60);
+      ctx.lineTo(500, 60);
       ctx.lineCap = 'butt';
       ctx.stroke();
-      ctx.closePath();
 
       ctx.beginPath();
-      ctx.moveTo(10, 60);
-      ctx.lineTo(110, 60);
+      ctx.moveTo(100, 120);
+      ctx.lineTo(500, 120);
       ctx.lineCap = 'square';
       ctx.stroke();
-      ctx.closePath();
 
       ctx.beginPath();
-      ctx.moveTo(10, 110);
-      ctx.lineTo(110, 110);
+      ctx.moveTo(100, 180);
+      ctx.lineTo(500, 180);
       ctx.lineCap = 'round';
       ctx.stroke();
-      ctx.closePath();
+
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = '#ff0000';
+
+      ctx.beginPath();
+      ctx.moveTo(75, 0);
+      ctx.lineTo(75, height);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(100, 0);
+      ctx.lineTo(100, height);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(0, 120);
+      ctx.lineTo(1000, 120);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(0, 60);
+      ctx.lineTo(1000, 60);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(0, 180);
+      ctx.lineTo(1000, 180);
+      ctx.stroke();
     }
   },
   false,
