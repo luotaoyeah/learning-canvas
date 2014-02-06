78a4103c252a713a7a24eb86061649709bca8b7c
luotao <luotaoyeah@gmail.com>
2014-02-06 20:28:25 +0800

    feat(html5-canvas): 02.02.the-basic-rectangle-shape

--- src/book/html5-canvas-2e/02.drawing-on-the-canvas/02.02.the-basic-rectangle-shape/01.js
+++ src/book/html5-canvas-2e/02.drawing-on-the-canvas/02.02.the-basic-rectangle-shape/01.js
@@ -4,10 +4,10 @@
 
 window.addEventListener(
   'load',
-  function() {
+  function () {
     var canvas = document.getElementById('canvas01');
     if (canvas && canvas.getContext) {
-      var ctx = (function(canvas) {
+      var ctx = (function (canvas) {
         var dpr = window.devicePixelRatio || 1;
         var rect = canvas.getBoundingClientRect();
         canvas.width = rect.width * dpr;
@@ -18,19 +18,15 @@ window.addEventListener(
       })(canvas);
       var width = canvas.width / (window.devicePixelRatio || 1);
       var height = canvas.height / (window.devicePixelRatio || 1);
-      console.log('--------------------------------------------------');
-
-      /*
-       * 在 canvas 中绘制一个 rectangle 有三种方式：
-       *     fillRect(x, y, width, height)
-       *     strokeRect(x, y, width, height)
-       *     clearRect(x, y, width, height)
-       */
 
       drawScreen(ctx, width, height);
     }
 
     /**
+     * 在 canvas 中绘制一个 rectangle 有三种方式:
+     *     fillRect(x, y, width, height)
+     *     strokeRect(x, y, width, height)
+     *     clearRect(x, y, width, height)
      *
      * @param {CanvasRenderingContext2D} ctx
      * @param width
@@ -38,19 +34,18 @@ window.addEventListener(
      */
     function drawScreen(ctx, width, height) {
       /*
-       * 对于 fillRect() 方法，
-       * 需要先设置 fillStyle 样式，即填充的颜色
+       * 对于 fillRect() 方法,
+       * 需要先设置 fillStyle 样式, 即填充的颜色
        */
       ctx.fillStyle = '#000000';
       ctx.fillRect(10, 10, 40, 40);
 
       /*
-       * 对于 strokeRect() 方法，
-       * 需要先设置 strokeStyle 样式，即线的颜色
-       * 以及 lineWidth 属性，即线的宽度
+       * 对于 strokeRect() 方法,
+       * 需要先设置 strokeStyle 样式, 即线的颜色,
+       * 以及 lineWidth 属性, 即线的宽度
        */
       ctx.strokeStyle = '#ff00ff';
-      ctx.lineWidth = 2;
       ctx.strokeRect(1, 1, 58, 58);
 
       ctx.clearRect(20, 20, 20, 20);
